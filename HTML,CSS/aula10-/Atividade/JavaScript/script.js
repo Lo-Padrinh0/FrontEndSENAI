6f642da0c92bac25dea711bd86caeee4807b30e0
Lo-Padrinh0 <166052100+Lo-Padrinh0@users.noreply.github.com>
2024-04-24 17:51:45 -0300

    aula10
    atividade

---
+++ HTML,CSS/aula10-/Atividade/JavaScript/script.js
@@ -0,0 +1,8 @@
+
+function MobileTrocar(){
+    if (menuMobile.style.flex.diretion == "column"){
+        menuMobile.style.flex.diretion = "row";
+    }else{
+        menuMobile.style.flex.diretion = "column";
+    }
+}
